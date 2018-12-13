eee85b0cb1423d98605b94ffdf8ff3475f0f8ba5
mattolu <mattolu@github>
2018-12-13 12:47:53 +0100

    card controller added

---
+++ app/Http/Controllers/CardController.php
@@ -0,0 +1,78 @@
+<?php
+namespace App\Http\Controllers;
+use App\User;
+use Illuminate\Http\Request;
+use App\User_subscription;
+use Validator;
+use Carbon\Carbon;
+
+class CardController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+    public function createCard(Request $request, $id)
+    {
+        
+        $validator = Validator::make($request->all(), [
+                'tariff_plan' => 'required',
+                //'start_date' => 'required',
+                // 'end_date' => 'required',
+                'amount' => 'required',
+        ]);
+
+        if($validator->fails()){
+            $response = array(  
+                                'response'=>$validator->messages(), 
+                                'success'=>false
+                            );
+            return $response;
+        }else{
+              
+                $user_subscription = new User_subscription();
+                $user_subscription->tariff_plan = $request->tariff_plan;
+                $user_subscription->start_date= Carbon::now();
+                $user_subscription->end_date= Carbon::now()->addMonths($request->tariff_plan);
+                $user_subscription->amount= $request->amount;
+                $user_subscription->user_id= User::find($id)->id;
+            
+                $user_subscription->save();
+               
+            }
+         
+        
+        if($user_subscription->save()){
+            $user_subscription = response()->json(
+                [
+                    'subscription' => [
+                        'posted' => true,
+                        'status' => 200,
+                        'message' => 'Subscription successful',
+                        'tariff_plan_in_month'=> $user_subscription->tariff_plan,
+                        'subscription_details' => $user_subscription
+                        
+                        ]
+                ], 201);
+                return $user_subscription;
+        }else{
+            $response = response()->json(
+                [
+                    'response' => [
+                        'posted' => false,
+                        'customized_message' => 'Subscription UNSUCCESSFUL',
+                        'status' => 401
+                        ]
+                ], 401);
+                return $response;
+        }
+        
+    }
+}
+
+   
